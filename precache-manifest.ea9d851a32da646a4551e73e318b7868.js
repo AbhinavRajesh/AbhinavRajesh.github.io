e57ccd54b3775f5b597466a63d1bcd6e34c00831
AbhinavRajesh <abhinavrajesh49@gmail.com>
2020-10-14 19:22:53 +0530

    Updates

---
+++ precache-manifest.ea9d851a32da646a4551e73e318b7868.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9401d6b5b54eb5a91552d8b68223cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c1ac92e7f6c00c91bfe",
+    "url": "/static/css/main.3281bd3e.chunk.css"
+  },
+  {
+    "revision": "35a92b3f81464e011489",
+    "url": "/static/js/2.c46e1748.chunk.js"
+  },
+  {
+    "revision": "d4b0302a1e882c57090284dfea2c6f01",
+    "url": "/static/js/2.c46e1748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1ac92e7f6c00c91bfe",
+    "url": "/static/js/main.d482437c.chunk.js"
+  },
+  {
+    "revision": "22c3aed477cdc0ed73a7",
+    "url": "/static/js/runtime-main.d6828ab7.js"
+  },
+  {
+    "revision": "7258e43265fa86492e90f9925176c7c4",
+    "url": "/static/media/AR Logo.7258e432.svg"
+  },
+  {
+    "revision": "23a3368038f7bbebd158f8e9949d41d8",
+    "url": "/static/media/About Image.23a33680.svg"
+  },
+  {
+    "revision": "6537851904825ed11a881ad416d0da1f",
+    "url": "/static/media/Landing Image.65378519.svg"
+  },
+  {
+    "revision": "2e563a8fc85a57a19746efb2915733d1",
+    "url": "/static/media/covid-19-tracker.2e563a8f.png"
+  },
+  {
+    "revision": "1756fb70b9487beb1218f22286e21100",
+    "url": "/static/media/jensonUSA.1756fb70.png"
+  },
+  {
+    "revision": "c58ed09c424ebcca6b1df990912fa458",
+    "url": "/static/media/octofetch.c58ed09c.png"
+  },
+  {
+    "revision": "087df1972bf1facfcd3595cd82d20cff",
+    "url": "/static/media/weather-pwa.087df197.png"
+  }
+]);
